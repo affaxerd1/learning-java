8b7aa84e600eb75b5d3bc8942155f17c096697a2
affaxerd1 <84396021+affaxerd1@users.noreply.github.com>
2023-08-21 23:03:09 +0300

    scheduling threads manually to run concurently

---
+++ schedule_threads/src/Main.java
@@ -0,0 +1,40 @@
+class A extends Thread{
+    public void run(){
+        for(int i=0; i <=100; i++){
+            System.out.println("hi");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class B extends Thread{
+    @Override
+    public void run(){
+        for(int i=0; i<=100; i++){
+            System.out.println("hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+public class Main{
+    public static void main(String[] args) {
+        A obj1=new A();
+        B obj2=new B();
+
+        obj1.start();
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        obj2.start();
+    }
+}
